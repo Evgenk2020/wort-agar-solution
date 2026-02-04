942990b0e007868e9eb87ec18dd2d4638095816a
evheny <evhenykopilov@ukr.net>
2026-02-04 15:25:27 +0200

    fix: main cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,59 +1,68 @@
 #include <iostream>
+#include <string>
+#include <cstdlib>
+
 #include "../include/output.h"
 
 int main(int argc, char *argv[])
 {
-    const std::string k_help_one = "-h";
-    const std::string k_help_two = "--help";
-    const std::string k_info = "-i";
-    const std::string k_data = "-d";
-    const std::string k_file = "-df";
-    std::string similar;
+    constexpr std::string_view k_help_one{"-h"};
+    constexpr std::string_view k_help_two{"--help"};
+    constexpr std::string_view k_info{"-i"};
+    constexpr std::string_view k_data{"-d"};
+    constexpr std::string_view k_file{"-df"};
 
-    if (argc >= 2)
+    if (argc < 2)
     {
-        similar = (std::string)argv[1];
+        std::cout << "error... use -h or --help for details\n";
+
+        return 1;
     }
+
+    const std::string_view arg{argv[1]};
 
     if (argc == 2)
     {
-        if (similar == k_help_one || similar == k_help_two)
+        if (arg == k_help_one || arg == k_help_two)
         {
-            print_info inf(new help_info);
-            inf._print();
+            print_info info(new help_info);
+            info._print();
+
+            return 0;
         }
 
-        if (similar == k_info)
+        if (arg == k_info)
         {
-            print_info inf(new inf_indo);
-            inf._print();
+            print_info info(new inf_indo);
+            info._print();
+
+            return 0;
         }
     }
 
-    else if (argc == 5 && similar == k_data || argc == 5 && similar == k_file)
+    if (argc == 5 && (arg == k_data || arg == k_file))
     {
-        wort_solution wort;
-        wort.first_wort = atof(argv[2]);
-        wort.finish_wort = atof(argv[3]);
-        wort.vol_filtrate = atof(argv[4]);
+        wort_solution wort{};
+        wort.first_wort = std::strtod(argv[2], nullptr);
+        wort.finish_wort = std::strtod(argv[3], nullptr);
+        wort.vol_filtrate = std::strtod(argv[4], nullptr);
 
-        if (similar == k_data)
+        if (arg == k_data)
         {
-            print_info inf(new screen_info);
-            inf._print(wort);
+            print_info info(new screen_info);
+            info._print(wort);
         }
 
-        if (similar == k_file)
+        else
         {
-            print_info inf(new file_info);
-            inf._print(wort);
+            print_info info(new file_info);
+            info._print(wort);
         }
+
+        return 0;
     }
 
-    else
-    {
-        std::cout << "error... use -h or --help for details" << std::endl;
-    }
-
-    return 0;
+    std::cout << "error... use -h or --help for details\n";
+    
+    return 1;
 }
